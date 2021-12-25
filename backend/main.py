a647b94da179fe0e43afd33db9200c55b9b8f81d
direcision <direcision@gmail.com>
2021-12-25 20:30:02 +0900

    Add dummpy item

--- backend/main.py
+++ backend/main.py
@@ -28,7 +28,9 @@ class Item(BaseModel):
 @app.get("/")
 def read_root():
     return [{"id": 1, "name": "도마",
-            "imageUrl": "https://shopping-phinf.pstatic.net/main_2867953/28679532555.20210908134006.jpg?type=f640"}]
+            "imageUrl": "https://shopping-phinf.pstatic.net/main_2867953/28679532555.20210908134006.jpg?type=f640"},
+            {"id": 2, "name": "도마2",
+            "imageUrl": "https://shopping-phinf.pstatic.net/main_2276628/22766289732.20210615105435.jpg?type=f640"}]
 
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Optional[str] = None):
