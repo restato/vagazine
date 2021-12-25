9da718952963b5e3af5132e1b09842fe2e0ef2f2
direcision <direcision@gmail.com>
2021-12-25 16:24:20 +0900

    FAdd api call function for fetching data

--- backend/main.py
+++ backend/main.py
@@ -2,9 +2,24 @@ from typing import Optional
 
 from fastapi import FastAPI
 from pydantic import BaseModel
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+origins = [
+    "http://localhost.tiangolo.com",
+    "https://localhost.tiangolo.com",
+    "http://localhost",
+    "http://localhost:8000",
+    "http://localhost:19006",
+]
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 class Item(BaseModel):
     name: str
     price: float
@@ -12,9 +27,8 @@ class Item(BaseModel):
 
 @app.get("/")
 def read_root():
-    return {"name": "도마",
-            "image_url": "https://shopping-phinf.pstatic.net/main_2867953/28679532555.20210908134006.jpg?type=f640"}
-
+    return [{"id": 1, "name": "도마",
+            "imageUrl": "https://shopping-phinf.pstatic.net/main_2867953/28679532555.20210908134006.jpg?type=f640"}]
 
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Optional[str] = None):
--- frontend/screens/TabOneScreen.tsx
+++ frontend/screens/TabOneScreen.tsx
@@ -1,20 +1,55 @@
+import React, { useEffect, useState } from 'react';
+import { ActivityIndicator, FlatList, Image } from 'react-native';
+
 import { StyleSheet } from 'react-native';
 import EditScreenInfo from '../components/EditScreenInfo';
 import { Text, View } from '../components/Themed';
 import { RootTabScreenProps } from '../types';
 
 export default function TabOneScreen({ navigation }: RootTabScreenProps<'TabOne'>) {
+
+  const [isLoading, setLoading] = useState(true);
+  const [data, setData] = useState([]);
+
+  const getData = async () => {
+    try {
+      // const url = 'https://reactnative.dev/movies.json';
+      const url = 'http://localhost:8000/';
+      const response = await fetch(url);
+      const json = await response.json();
+      setData(json) // json.movies
+    } catch (error) {
+      console.error(error);
+    } finally {
+      setLoading(false);
+    }
+  }
+
+  useEffect(() => {
+    getData();
+  }, []);
+
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Tab One</Text>
       <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <Text> Unknown </Text>
       {/* <EditScreenInfo path="/screens/TabOneScreen.tsx" /> */}
-
-      http://localhost:8000/
-
-
-    </View>
+      <View style={{ flex: 1, padding: 24 }}>
+        {isLoading ? <ActivityIndicator/> : (
+          <FlatList
+            data={data}
+            keyExtractor={({ id }, index) => id}
+            renderItem={({ item }) => (
+              <View>
+                <Text>{item.name}</Text>
+                <Image source={item.imageUrl}
+                       style={{width: 400, height: 400}} />
+              </View>
+            )}
+          />
+        )}
+      </View>
+  </View>
   );
 }
 
