4490e74b00c2240d17012e8fd75cf83683e18448
vagazine <vagazine88@gmail.com>
2021-12-26 23:55:10 +0900

    Add test code

--- backend/util/scrap.py
+++ backend/util/scrap.py
@@ -2,6 +2,9 @@ import requests
 from bs4 import BeautifulSoup
 import urllib.request
 
+def test():
+    print("GG")
+
 def do(url):
     headers = {"User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.93 Safari/537.36"}
     response = requests.get(url, headers=headers)
