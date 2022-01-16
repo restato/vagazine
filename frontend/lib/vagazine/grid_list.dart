31500df9cde3d7d1640e713261f1299b2a7a6cc1
vagazine <vagazine88@gmail.com>
2022-01-16 23:50:07 +0900

    Load fethItems to GridList

--- frontend/lib/vagazine/grid_list.dart
+++ frontend/lib/vagazine/grid_list.dart
@@ -1,8 +1,53 @@
 import 'package:flutter/material.dart';
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+
+class Item {
+  final int id;
+  final String title;
+  final String price;
+  final String imageUrl;
+
+  Item({
+    required this.id,
+    required this.title,
+    required this.price,
+    required this.imageUrl,
+  });
+
+  factory Item.fromJson(Map<String, dynamic> json) {
+    return Item(
+      id: json['id'],
+      title: json['title'],
+      price: json['price'],
+      imageUrl: json['imageUrl'],
+    );
+  }
+}
+
+Future<List<Item>> fetchItems() async {
+  final response = await http.get(Uri.parse('https://dongsan.club/items'));
+  if (response.statusCode == 200) {
+    // If the server did return a 200 OK response,
+    // then parse the JSON.
+    List<Item> items = [];
+    List<dynamic> itemsJson = jsonDecode(response.body);
+    itemsJson.forEach(
+      (oneItem) {
+        Item item = Item.fromJson(oneItem);
+        items.add(item);
+      },
+    );
+    return items;
+  } else {
+    // If the server did not return a 200 OK response,
+    // then throw an exception.
+    throw Exception('Failed to load album');
+  }
+}
 
 class GridListDemo extends StatelessWidget {
   const GridListDemo({Key? key}) : super(key: key);
-
   List<_Photo> _photos(BuildContext context) {
     return [
       _Photo(
@@ -21,18 +66,40 @@ class GridListDemo extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: GridView.count(
-        restorationId: 'grid_view_demo_grid_offset',
-        crossAxisCount: 2,
-        mainAxisSpacing: 8,
-        crossAxisSpacing: 8,
-        padding: const EdgeInsets.all(8),
-        childAspectRatio: 1,
-        children: _photos(context).map<Widget>((photo) {
-          return _GridPhotoItem(photo: photo);
-        }).toList(),
-      ),
-    );
+        home: FutureBuilder<List<Item>>(
+            future: fetchItems(),
+            builder: (context, snapshot) {
+              if (snapshot.hasData) {
+                List<Item>? resData = snapshot.data;
+                return GridView.builder(
+                    itemCount: resData != null ? resData.length : 0,
+                    gridDelegate:
+                        const SliverGridDelegateWithFixedCrossAxisCount(
+                      crossAxisCount: 3,
+                    ),
+                    itemBuilder: (context, index) {
+                      Item item = resData![index];
+                      return _GridPhotoItem(item: item);
+                    });
+              } else if (snapshot.hasError) {
+                return Text("err");
+              }
+              return const CircularProgressIndicator();
+            })
+        // home: GridView.count(
+        //     restorationId: 'grid_view_demo_grid_offset',
+        //     crossAxisCount: 2,
+        //     mainAxisSpacing: 8,
+        //     crossAxisSpacing: 8,
+        //     padding: const EdgeInsets.all(8),
+        //     childAspectRatio: 1,
+        // children: _photos(context).map<Widget>((photo) {
+        //   return _GridPhotoItem(photo: photo);
+        // }).toList(),
+        // children: FutureBuilder<List<Item>>(
+        // builder: (context, snapshot) {},
+        // )),
+        );
   }
 }
 
@@ -65,10 +132,11 @@ class _GridTitleText extends StatelessWidget {
 class _GridPhotoItem extends StatelessWidget {
   const _GridPhotoItem({
     Key? key,
-    required this.photo,
+    required this.item,
   }) : super(key: key);
 
-  final _Photo photo;
+  // final _Photo photo;
+  final Item item;
 
   @override
   Widget build(BuildContext context) {
@@ -76,7 +144,7 @@ class _GridPhotoItem extends StatelessWidget {
       shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(4)),
       clipBehavior: Clip.antiAlias,
       child: Image.network(
-        photo.imageUrl,
+        item.imageUrl,
         fit: BoxFit.cover,
       ),
     );
@@ -90,8 +158,8 @@ class _GridPhotoItem extends StatelessWidget {
         clipBehavior: Clip.antiAlias,
         child: GridTileBar(
           backgroundColor: Colors.black45,
-          title: _GridTitleText(photo.title),
-          subtitle: _GridTitleText(photo.subtitle),
+          title: _GridTitleText(item.title),
+          subtitle: _GridTitleText(item.price),
         ),
       ),
       child: image,
