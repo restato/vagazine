4daa9a0892203c3c0eed8c815d1f4398fdc295c8
vagazine <vagazine88@gmail.com>
2022-01-16 21:29:53 +0900

    Add Fetch Data Example (FutureAlbums)

--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -1,4 +1,101 @@
-import 'package:flutter/material.dart';
-import 'vagazine/app.dart';
+// import 'package:flutter/material.dart';
+// import 'vagazine/app.dart';
 
-void main() => runApp(const VagazineApp());
+// void main() => runApp(const VagazineApp());
+import 'dart:async';
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
+Future<List<Album>> fetchAlbum() async {
+  final response =
+      await http.get(Uri.parse('https://jsonplaceholder.typicode.com/albums'));
+
+  if (response.statusCode == 200) {
+    // If the server did return a 200 OK response,
+    // then parse the JSON.
+    List<Album> albums = [];
+    List<dynamic> albumsJson = jsonDecode(response.body);
+    albumsJson.forEach(
+      (oneAlbum) {
+        Album album = Album.fromJson(oneAlbum);
+        albums.add(album);
+      },
+    );
+    return albums;
+  } else {
+    // If the server did not return a 200 OK response,
+    // then throw an exception.
+    throw Exception('Failed to load album');
+  }
+}
+
+class Album {
+  final int userId;
+  final int id;
+  final String title;
+
+  Album({
+    required this.userId,
+    required this.id,
+    required this.title,
+  });
+
+  factory Album.fromJson(Map<String, dynamic> json) {
+    return Album(
+      userId: json['userId'],
+      id: json['id'],
+      title: json['title'],
+    );
+  }
+}
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatefulWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Fetch Data Example',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Fetch Data Example'),
+        ),
+        body: Center(
+          child: FutureBuilder<List<Album>>(
+            future: fetchAlbum(),
+            builder: (context, snapshot) {
+              if (snapshot.hasData) {
+                List<Album>? resData = snapshot.data;
+                return ListView.builder(
+                    itemCount: resData != null ? resData.length : 0,
+                    itemBuilder: (context, index) {
+                      return Card(
+                        child: ListTile(
+                          title: Text(resData?[index].title ?? ""),
+                        ),
+                      );
+                    });
+              } else if (snapshot.hasError) {
+                return Text('${snapshot.error}');
+              }
+              // By default, show a loading spinner.
+              return const CircularProgressIndicator();
+            },
+          ),
+        ),
+      ),
+    );
+  }
+}
