a5420818a013666be8da0f98a35207e85ccb9da8
vagazine <vagazine88@gmail.com>
2022-01-20 21:53:18 +0900

    Add best flutter ui home

--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -1,19 +1,70 @@
+import 'dart:io';
+import 'navigation.dart';
+import 'themes/app_theme.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/foundation.dart' show kIsWeb;
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await SystemChrome.setPreferredOrientations(<DeviceOrientation>[
+    DeviceOrientation.portraitUp,
+    DeviceOrientation.portraitDown
+  ]).then((_) => runApp(MyApp()));
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(
+      statusBarColor: Colors.transparent,
+      statusBarIconBrightness: Brightness.dark,
+      statusBarBrightness:
+          !kIsWeb && Platform.isAndroid ? Brightness.dark : Brightness.light,
+      systemNavigationBarColor: Colors.white,
+      systemNavigationBarDividerColor: Colors.transparent,
+      systemNavigationBarIconBrightness: Brightness.dark,
+    ));
+    return MaterialApp(
+      title: 'Vagazine',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        textTheme: AppTheme.textTheme,
+        platform: TargetPlatform.iOS,
+      ),
+      home: NavigationHomeScreen(),
+    );
+  }
+}
+
+// class HexColor extends Color {
+//   HexColor(final String hexColor) : super(_getColorFromHex(hexColor));
+
+//   static int _getColorFromHex(String hexColor) {
+//     hexColor = hexColor.toUpperCase().replaceAll('#', '');
+//     if (hexColor.length == 6) {
+//       hexColor = 'FF' + hexColor;
+//     }
+//     return int.parse(hexColor, radix: 16);
+//   }
+// }
+
 
 // import 'vagazine/app.dart';
 // void main() => runApp(const VagazineApp());
 
 // import 'vagazine/grid_list.dart';
 // void main() => runApp(const GridListDemo());
-import 'vagazine/motion.dart';
-import 'screens/home.dart';
-import 'themes/light.theme.dart';
+// import 'vagazine/motion.dart';
+// import 'screens/home.dart';
+// import 'themes/light.theme.dart';
 
-void main() => runApp(MaterialApp(
-      title: 'vagazine',
-      home: const Home(),
-      theme: lightTheme(),
-    ));
+// void main() => runApp(MaterialApp(
+//       title: 'vagazine',
+//       home: const Home(),
+//       theme: lightTheme(),
+//     ));
 
 // import 'dart:async';
 // import 'dart:convert';
