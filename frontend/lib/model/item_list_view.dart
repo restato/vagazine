2a58f825970e3c2af647b35257c5471ab7f0be5a
vagazine <vagazine88@gmail.com>
2022-03-08 09:28:04 +0900

    Fix main ui

--- frontend/lib/model/item_list_view.dart
+++ frontend/lib/model/item_list_view.dart
@@ -110,102 +110,106 @@ class ItemListView extends StatelessWidget {
                                                   ))
                                             ]),
                                             // price
-                                            getBottomView(context, itemData),
-                                            Padding(
-                                              padding:
-                                                  const EdgeInsets.only(top: 4),
-                                              child: Row(
-                                                children: <Widget>[
-                                                  // RatingBar(
-                                                  //   initialRating: 5,
-                                                  //   direction:
-                                                  //       Axis.horizontal,
-                                                  //   allowHalfRating: true,
-                                                  //   itemCount: 5,
-                                                  //   itemSize: 24,
-                                                  //   ratingWidget:
-                                                  //       RatingWidget(
-                                                  //     full: Icon(
-                                                  //       Icons
-                                                  //           .star_rate_rounded,
-                                                  //       color: HotelAppTheme
-                                                  //               .buildLightTheme()
-                                                  //           .primaryColor,
-                                                  //     ),
-                                                  //     half: Icon(
-                                                  //       Icons
-                                                  //           .star_half_rounded,
-                                                  //       color: HotelAppTheme
-                                                  //               .buildLightTheme()
-                                                  //           .primaryColor,
-                                                  //     ),
-                                                  //     empty: Icon(
-                                                  //       Icons
-                                                  //           .star_border_rounded,
-                                                  //       color: HotelAppTheme
-                                                  //               .buildLightTheme()
-                                                  //           .primaryColor,
-                                                  //     ),
-                                                  //   ),
-                                                  //   itemPadding:
-                                                  //       EdgeInsets.zero,
-                                                  //   onRatingUpdate:
-                                                  //       (rating) {
-                                                  //     print(rating);
-                                                  //   },
-                                                  // ),
-                                                  // TextButton(
-                                                  //   // style: TextButton
-                                                  //   //     .styleFrom(
-                                                  //   //   padding:
-                                                  //   //       const EdgeInsets
-                                                  //   //           .all(0.0),
-                                                  //   //   textStyle:
-                                                  //   //       const TextStyle(
-                                                  //   //           fontSize: 20),
-                                                  //   // ),
-                                                  //   onPressed: () {
-                                                  //     launchURL(
-                                                  //         itemData!.url);
-                                                  //   },
-                                                  //   child:
-                                                  //       const Text('구매하기'),
-                                                  // ),
-                                                ],
-                                              ),
-                                            ),
+                                            Container(
+                                                child: getBottomView(
+                                                    context, itemData),
+                                                alignment:
+                                                    Alignment.centerRight),
+                                            // Padding(
+                                            //   padding:
+                                            //       const EdgeInsets.only(top: 4),
+                                            //   child: Row(
+                                            //     children: <Widget>[
+                                            //       // RatingBar(
+                                            //       //   initialRating: 5,
+                                            //       //   direction:
+                                            //       //       Axis.horizontal,
+                                            //       //   allowHalfRating: true,
+                                            //       //   itemCount: 5,
+                                            //       //   itemSize: 24,
+                                            //       //   ratingWidget:
+                                            //       //       RatingWidget(
+                                            //       //     full: Icon(
+                                            //       //       Icons
+                                            //       //           .star_rate_rounded,
+                                            //       //       color: HotelAppTheme
+                                            //       //               .buildLightTheme()
+                                            //       //           .primaryColor,
+                                            //       //     ),
+                                            //       //     half: Icon(
+                                            //       //       Icons
+                                            //       //           .star_half_rounded,
+                                            //       //       color: HotelAppTheme
+                                            //       //               .buildLightTheme()
+                                            //       //           .primaryColor,
+                                            //       //     ),
+                                            //       //     empty: Icon(
+                                            //       //       Icons
+                                            //       //           .star_border_rounded,
+                                            //       //       color: HotelAppTheme
+                                            //       //               .buildLightTheme()
+                                            //       //           .primaryColor,
+                                            //       //     ),
+                                            //       //   ),
+                                            //       //   itemPadding:
+                                            //       //       EdgeInsets.zero,
+                                            //       //   onRatingUpdate:
+                                            //       //       (rating) {
+                                            //       //     print(rating);
+                                            //       //   },
+                                            //       // ),
+                                            //       // TextButton(
+                                            //       //   // style: TextButton
+                                            //       //   //     .styleFrom(
+                                            //       //   //   padding:
+                                            //       //   //       const EdgeInsets
+                                            //       //   //           .all(0.0),
+                                            //       //   //   textStyle:
+                                            //       //   //       const TextStyle(
+                                            //       //   //           fontSize: 20),
+                                            //       //   // ),
+                                            //       //   onPressed: () {
+                                            //       //     launchURL(
+                                            //       //         itemData!.url);
+                                            //       //   },
+                                            //       //   child:
+                                            //       //       const Text('구매하기'),
+                                            //       // ),
+                                            //     ],
+                                            //   ),
+                                            // ),
                                           ],
                                         ),
                                       ),
                                     ),
                                   ),
-                                  Padding(
-                                    padding: const EdgeInsets.only(
-                                        right: 16, top: 8),
-                                    child: Column(
-                                      mainAxisAlignment:
-                                          MainAxisAlignment.center,
-                                      crossAxisAlignment:
-                                          CrossAxisAlignment.end,
-                                      children: <Widget>[
-                                        // Text(
-                                        //   '\$${hotelData!.perNight}',
-                                        //   textAlign: TextAlign.left,
-                                        //   style: TextStyle(
-                                        //     fontWeight: FontWeight.w600,
-                                        //     fontSize: 22,
-                                        //   ),
-                                        // ),
-                                        // Text(
-                                        //   '/per night',
-                                        //   style: TextStyle(
-                                        //       fontSize: 14,
-                                        //       color:
-                                        //           Colors.grey.withOpacity(0.8)),
-                                        // ),
-                                      ],
-                                    ),
-                                  ),
+                                  // Padding(
+                                  //   padding: const EdgeInsets.only(
+                                  //       right: 16, top: 8),
+                                  //   child: Column(
+                                  //     mainAxisAlignment:
+                                  //         MainAxisAlignment.center,
+                                  //     crossAxisAlignment:
+                                  //         CrossAxisAlignment.end,
+                                  //     children: <Widget>[
+                                  //       // Text(
+                                  //       //   '\$${hotelData!.perNight}',
+                                  //       //   textAlign: TextAlign.left,
+                                  //       //   style: TextStyle(
+                                  //       //     fontWeight: FontWeight.w600,
+                                  //       //     fontSize: 22,
+                                  //       //   ),
+                                  //       // ),
+                                  //       // Text(
+                                  //       //   '/per night',
+                                  //       //   style: TextStyle(
+                                  //       //       fontSize: 14,
+                                  //       //       color:
+                                  //       //           Colors.grey.withOpacity(0.8)),
+                                  //       // ),
+                                  //     ],
+                                  //   ),
+                                  // ),
                                 ],
                               ),
                             ),
