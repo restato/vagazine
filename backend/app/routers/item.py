67089ae94a6a2e27edc7c73af36a9f69ce07f8f7
restato <direcision@gmail.com>
2022-03-13 00:42:34 +0900

    [#9] Modify get_items def as async

--- backend/app/routers/item.py
+++ backend/app/routers/item.py
@@ -6,7 +6,7 @@ router_items = APIRouter(prefix='/items', tags=['items'])
 
 @router_items.get('/', summary='List of Items', description='return all items')
 @cache(expire=60)
-def get_items():
+async def get_items():
     items = misc.read_yaml('data/for-baby-food.yaml')
     for idx, item in enumerate(items):
         data = {}
